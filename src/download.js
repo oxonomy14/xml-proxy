c3b88ce909c78d8bacf70810682148bff501435c
Andrii Semenenko <oxonomy14@gmail.com>
2025-02-07 20:48:20 +0700

    zzzssss

--- src/download.js
+++ src/download.js
@@ -14,7 +14,7 @@ import puppeteer from 'puppeteer';
 
   const xmlContent = await page.evaluate(() => document.body.innerText);
 
-  // fs.writeFileSync('exprates.xml', xmlContent);
+  fs.writeFileSync('exprates.xml', xmlContent);
   fs.writeFileSync(
     'data.xml',
     `<updated>${new Date().toISOString()}</updated>`
