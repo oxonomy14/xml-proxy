c364cb4d09936996e34cae36b06d82ddb046d20c
Andrii Semenenko <oxonomy14@gmail.com>
2025-02-07 21:21:17 +0700

    mmmm

--- src/download.js
+++ src/download.js
@@ -1,8 +1,6 @@
-//const puppeteer = require('puppeteer');
-//const fs = require('fs');
-
 import fs from 'fs';
 import puppeteer from 'puppeteer';
+import path from 'path';
 
 (async () => {
   const browser = await puppeteer.launch({
@@ -18,9 +16,23 @@ import puppeteer from 'puppeteer';
 
   const xmlContent = await page.evaluate(() => document.body.innerText);
 
-  fs.writeFileSync('exprates.xml', xmlContent);
+  // Указываем директорию src для сохранения файла
+  const directoryPath = path.join(__dirname, 'src'); // Путь к директории src
+
+  // Проверка и создание директории src, если её нет
+  if (!fs.existsSync(directoryPath)) {
+    fs.mkdirSync(directoryPath, { recursive: true });
+  }
+
+  // Путь к файлу внутри src
+  const filePath = path.join(directoryPath, 'exprates.xml');
+
+  // Записываем XML в файл
+  fs.writeFileSync(filePath, xmlContent);
+
+  // Записываем обновлённую метку времени в data.xml
   fs.writeFileSync(
-    'data.xml',
+    path.join(directoryPath, 'data.xml'),
     `<updated>${new Date().toISOString()}</updated>`
   );
 
